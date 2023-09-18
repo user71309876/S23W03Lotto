2ae9d2c75eb09736df394cf3515513ab232bba32
박재현 <parkjaehyeon@bagjaehyeon-ui-MacBookAir.local>
2023-09-18 15:18:24 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/kumoh/ce/s20180474/s23w03lotto/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.ce.s20180474.s23w03lotto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
