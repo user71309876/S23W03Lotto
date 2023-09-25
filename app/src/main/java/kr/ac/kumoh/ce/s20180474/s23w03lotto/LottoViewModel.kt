4cdb7fa84dbe1c3b7b59eba064eb3c1fdca0eaa9
parkjaehyeon <yongdong1205@gmail.com>
2023-09-25 15:21:34 +0900

    second

---
+++ app/src/main/java/kr/ac/kumoh/ce/s20180474/s23w03lotto/LottoViewModel.kt
@@ -0,0 +1,4 @@
+package kr.ac.kumoh.ce.s20180474.s23w03lotto
+
+class LottoViewModel {
+}
--- app/src/main/java/kr/ac/kumoh/ce/s20180474/s23w03lotto/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20180474/s23w03lotto/MainActivity.kt
@@ -2,6 +2,7 @@ package kr.ac.kumoh.ce.s20180474.s23w03lotto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kr.ac.kumoh.ce.s20180474.s23w03lotto.databinding.ActivityMainBinding
 import kotlin.random.Random
 
@@ -9,6 +10,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var main: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.i("Lifecycle!!!","onCreate()")
         //setContentView(R.layout.activity_main)
         main = ActivityMainBinding.inflate(layoutInflater)
         setContentView(main.root)
@@ -21,6 +23,36 @@ class MainActivity : AppCompatActivity() {
             main.num5.text = Random.nextInt(1, 46).toString()
             main.num6.text = Random.nextInt(1, 46).toString()
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("Lifecycle!!!","onStart()")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("Lifecycle!!!","onResume()")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("Lifecycle!!!","onPause()")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("Lifecycle!!!","onStop()")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("Lifecycle!!!","onDestroy()")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("Lifecycle!!!","onRestart()")
     }
 }
 
